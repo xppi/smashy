e0c58eccbad59eb9777d93f1de7e1227b06f8a30
xppi <59060461+xppi@users.noreply.github.com>
2024-02-13 17:14:19 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 import { PublicKey } from '@solana/web3.js'
 
 // Can be configured in .env
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://rpc.helius.xyz/?api-key=e35cb8cf-ba28-4b2c-a685-5ac0f9753b53"
 
 // Change this value to your Solana address
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey('A1yPM7RjvUetd6Fq8VRTatPSsidqWWwydascNShkVc91')
