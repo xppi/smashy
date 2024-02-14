f0ae1d7a09420902108ba652a57898de60ab3690
xppi <59060461+xppi@users.noreply.github.com>
2024-02-14 13:01:36 +0100

    up

--- src/sections/Airdrop/Airdrop.tsx
+++ src/sections/Airdrop/Airdrop.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { GameSlider } from "../Dashboard/Dashboard";
+import { useToast } from "../../hooks/useToast"; // Adjust the path as necessary
 
 const baseTextStyles = css`
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   color: #f1f1f1;
-  margin: 0;
-  padding: 0.25em 0;
+
 `;
 
 const Wrapper = styled.div`
   text-align: center;
   border-radius: 14px;
-  padding: 20px;
+  padding: 20px 0 20px 0;
   ${baseTextStyles};
   background: #33333361; /* Subtle background for depth, replace with desired non-color style if needed */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Soft shadow for depth */
@@ -42,11 +42,11 @@ const Text = styled.p`
 const listStyles = css`
   text-align: left;
   display: inline-block;
-  margin: auto;
+  margin: 10px;
   padding-left: 20px; /* Ensure alignment */
 `;
 
-const List = styled.ol`
+const List = styled.ul`
   ${listStyles};
 `;
 
@@ -63,8 +63,29 @@ const TipsItem = styled.li`
   ${baseTextStyles};
   margin-bottom: 10px;
 `;
+const ClaimButton = styled.button`
+margin: 20px;
+font-size: 30px;
+background: #0b3d3830;
+color: #ffe182;
+font-weight: 1000;
+border: 2px solid #29cbbe14;
+border-bottom: 4px solid #0b161c;
+border-radius: 5px;
+cursor: pointer;
+`;
 
 export default function Airdrop() {
+  const toast = useToast(); // Initialize the useToast hook
+
+  // Function to call when CLAIM AIRDROP button is clicked
+  const handleClaimClick = () => {
+    // Use the toast function from useToast hook to display the notification
+    toast({
+      title: '📋 AIRDROP STILL ONGOING!',
+      description: 'Wager or take part in our Zealay & Discord quests!',
+    })
+  };
   return (
     <>
       <Wrapper>
@@ -72,42 +93,24 @@ export default function Airdrop() {
         <Subtitle>Earn Airdrop Points on smashy.gg while playing your favorite games!</Subtitle>
 
         <Text>
-          Welcome to the $SMSH Airdrop program on smashy.gg! Play and wager to
+          Welcome to the $SMSH Airdrop program on smashy.gg <br /> Play and wager to
           collect more Airdrop points and earn fantastic rewards.
         </Text>
 
         <Subtitle>How to Earn Airdrop Points:</Subtitle>
         <List>
-          <ListItem>Play games on smashy.gg and complete challenges.</ListItem>
+          <ListItem>Play games on smashy.gg</ListItem>
           <ListItem>
-            Wager on your favorite games to earn additional Airdrop points.
+            Complete Zealay quests <a href="https://zealay.io"> here</a>
           </ListItem>
-          <ListItem>Participate in special events and promotions on the platform.</ListItem>
+          <ListItem>Participate in special events and promotions on our discord.</ListItem>
         </List>
 
         <Subtitle>How Does the Airdrop Work?</Subtitle>
         <Text>
-          The more you play and wager, the more Airdrop points you accumulate. <br />
-          These points can be redeemed for various rewards, such as exclusive
-          in-game items, discounts, and much more!
+          More informations will be announced soon, stay tuned!
         </Text>
-
-        <Subtitle>Tips for Maximum Airdrop Points:</Subtitle>
-        <TipsList>
-          <TipsItem>
-            Explore different games and challenges to maximize your points.
-          </TipsItem>
-          <TipsItem>Don't miss out on special promotions and events on smashy.gg.</TipsItem>
-          <TipsItem>
-            Stay active and engaged to continuously collect Airdrop points.
-          </TipsItem>
-        </TipsList>
-
-        <Text>
-          Ready to get started? Select a game below and start playing!
-        </Text>
-
-        <Text>Have fun and happy gaming!</Text>
+        <ClaimButton onClick={handleClaimClick}>CLAIM AIRDROP</ClaimButton>
       </Wrapper>
       <GameSlider />
     </>
